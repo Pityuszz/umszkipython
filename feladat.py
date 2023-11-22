1d47bbba26f2fb6f51ec414ea1eaf838b2633cc8
pit <peter.istvan.janser@umszki.hu>
2023-11-22 13:53:47 +0100

    Finished "1. feladat"

--- feladat.py
+++ feladat.py
@@ -7,6 +7,14 @@ egyseg = ["kg", "450 g", "400 g", "kg", "db", "250 g", "200 g", "1 kg", "1 kg", 
 # ▪     Ha igen, akkor állapítsa meg az indexét és írja ki a kiszerelés egységét!
 # ▪     Ha nem, akkor a parancssoron a „Nincs ilyen termék” üzenet jelenjen meg!
 
+termeknev = input("Keresett termék neve: ")
+if termeknev in termek:
+    index = termek.index(termeknev)
+    kiszereles = egyseg[index]
+    print(kiszereles)
+else:
+    print("Nincs ilyen termék")
+
 december = [ 2550, 1602, 1140, 1620, 1320, 682, 750, 1700, 1000, 693, 651, 686, 805, 702, 600, 582, 798, 648, 522, 605, 550, 567, 286, 350, 351, 250, 154, 135, 50 ]
 november = [ 2400, 1800, 1500, 1500, 1500, 1100, 1000, 1000, 1000, 770, 700, 700, 700, 600, 600, 600, 600, 600, 580, 550, 440, 540, 440, 350, 270, 250, 220, 150, 80 ]
 
